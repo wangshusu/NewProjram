7982c0b4988931c96f63f8b4d52394396dcffd11
王振兴 <1750239165@qq.com>
2023-02-05 15:28:23 +0800

    close

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 
 int main() {
     int fd = open("./1.txt", O_RDONLY);
+    if (fd == -1) {
+        fprintf(stderr, "open errno !\n reason : %s\n", strerror(errno));
+    }
+
+    close(fd);
     return 1;
 }

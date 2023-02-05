2f90a2b5a9bb62ed2bd96e7a48bbdcbb7fc3a4c0
王振兴 <1750239165@qq.com>
2023-02-05 18:01:37 +0800

    read 函数 printf 输出读取的内容

--- main.c
+++ main.c
@@ -5,11 +5,21 @@
 #include <unistd.h>
 #include <errno.h>
 
+#define MAX_BUF_NUMBER 1024
+
 int main() {
     int fd = open("./1.txt", O_RDONLY);
     if (fd == -1) {
         fprintf(stderr, "open errno !\n reason : %s\n", strerror(errno));
     }
+
+    char buf[MAX_BUF_NUMBER] = {0};
+    int ret = read(fd, buf, sizeof(buf));
+    if (ret < 0) {
+        fprintf(stderr, "read error!\n");
+    }
+
+
 
     close(fd);
     return 1;
